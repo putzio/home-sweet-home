c7acc7453be8cd20e90e10d89caeb7b2cd7107ed
putzio <krzys.p9@gmail.com>
2023-12-17 00:07:40 +0100

    pre-commit

--- README.md
+++ README.md
@@ -4,14 +4,35 @@
 You need to install:
 - git
 - python3
-- 
+- mysql
 
 ## Prepare enviroment 
 
-run startup.sh script. it will set up virtual enviroment and download all of the necessary packages
+On linux simply run the startup.sh script. it will set up virtual enviroment (reccomended) and download all of the necessary packages. 
 
 ## Setup MySQL
 
 ### On linux (apt based e.g. mint)
 
-Follow this tutorial: 'https://linuxhint.com/install-mysql-linux-mint-ubuntu/'
+Follow this tutorial: `https://linuxhint.com/install-mysql-linux-mint-ubuntu/`
+
+## Using precommit
+
+### Instalation
+If you haven`t runned the `startup.sh`:
+0. Install python3
+1. Install precommit using `pip install pre-commit`
+2. Instal git hook scripts: `pre-commit install`
+
+### Usage
+In our case it will simply check if the change files are properly formatted (using flake8) and format it if neccesary (using black) every time you commit changes. 
+
+You can also always check all of the project files by running `pre-commit run --all-files`.
+
+For more info check out `https://pre-commit.com/``
+
+
+### Flake8 config
+The flake8 configuration is stored in `.flake8` file
+### Black Formatter config
+The black formatter config are stored in `pyproject.toml` file
--- web/app.py
+++ web/app.py
@@ -1,45 +1,36 @@
 from flask import Flask, render_template, request, jsonify
+
 # import ngrok
-from datetime import datetime 
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return render_template('index.html', current_datetime=current_datetime)
+    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render_template("index.html", current_datetime=current_datetime)
 
-@app.route('/add_alarm')
+
+@app.route("/add_alarm")
 def add_alarm():
-    current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     alarms = [
-        {
-            'id': 1,
-            'time': '2021-01-01 12:00:00',
-            'description': 'Wake up'
-        },
-        {
-            'id': 2,
-            'time': '2021-01-01 12:00:00',
-            'description': 'Take medicine'
-        },
-        {
-            'id': 3,
-            'time': '2021-01-01 12:00:00',
-            'description': 'Go to sleep'
-        }
+        {"id": 1, "time": "2021-01-01 12:00:00", "description": "Wake up"},
+        {"id": 2, "time": "2021-01-01 12:00:00", "description": "Take medicine"},
+        {"id": 3, "time": "2021-01-01 12:00:00", "description": "Go to sleep"},
     ]
-    return render_template('add_alarm.html', current_datetime=current_datetime, alarms=alarms)
+    return render_template("add_alarm.html", current_datetime=current_datetime, alarms=alarms)
 
 
-@app.route('/api/set-color', methods=['POST'])
+@app.route("/api/set-color", methods=["POST"])
 def set_color():
     data = request.get_json()
     # Process the RGB values (data['red'], data['green'], data['blue'])
     # Implement your logic to control the RGB lights here
 
     # For demonstration purposes, just echoing the received data
-    return jsonify({'message': f'Color set to RGB({data["red"]}, {data["green"]}, {data["blue"]})'})
+    return jsonify({"message": f'Color set to RGB({data["red"]}, {data["green"]}, {data["blue"]})'})
 
 
 # def foreward_port(port: int):
@@ -49,6 +40,6 @@ def set_color():
 #     print(f"Ingress established at: {listener.url()}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # foreward_port(5000)
     app.run(debug=True)
